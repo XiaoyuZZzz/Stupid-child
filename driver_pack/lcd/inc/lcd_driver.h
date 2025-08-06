0866dc1feec89aa1271f170f039b67e86d5c2500
SuperYu <824229900@qq.com>
2025-08-07 00:31:03 +0800

    update .gitgnore

---
+++ driver_pack/lcd/inc/lcd_driver.h
@@ -0,0 +1,11 @@
+#ifndef LCD_DRIVER_H__
+#define LCD_DRIVER_H__
+
+
+#include "sc_conf.h"
+
+
+
+
+
+#endif
---
+++ driver_pack/lcd/src/lcd_driver.c
@@ -0,0 +1,11 @@
+/*
+ * @file         : lcd_driver.c
+ * @Author       : shuyu
+ * @LastEditTime : 2025-08-03 21:45
+ * @Description  : lcd_driver的驱动文件
+ */
+#include "lcd_driver.h"
+
+
+
+ 
